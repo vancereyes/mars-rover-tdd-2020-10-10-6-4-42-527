f64eca443ee54d5f19420e7e8d0f81c6ca1d3fb9
VANCE ALBERT REYES (SHP-IRIS-ISD-OOCLL/MNL) <REYESVA@OOCL.COM>
2020-10-15 17:06:46 +0800

    [MarsRover][Vance]test - created test for 00N M

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,18 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
-
+    void should_have_x_0_y_1_heading_N_when_move_M_given_x_0_y_0_heading_N() {
+        //given
+        Application marsRover = new Application();
+        //when
+        marsRover.executesCommand("M");
+        //then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(1, marsRover.getLocationY());
+        assertEquals("N", marsRover.getHeading());
     }
 }
