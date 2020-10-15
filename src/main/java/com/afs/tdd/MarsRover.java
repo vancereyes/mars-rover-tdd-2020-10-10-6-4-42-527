a62f03b07409baf5bdb4b239a6a7b95050e75c19
VANCE ALBERT REYES (SHP-IRIS-ISD-OOCLL/MNL) <REYESVA@OOCL.COM>
2020-10-15 18:00:38 +0800

    [MarsRover][Vance] refactor - refactored strings in first 1 features

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -4,6 +4,11 @@ import java.util.Arrays;
 
 public class MarsRover {
 
+    public static final String MOVE = "M";
+    public static final String LEFT = "L";
+    public static final String RIGHT = "R";
+    public static final String NORTH = "N";
+    public static final String WEST = "W";
     private int locationX;
     private int locationY;
     private String heading;
@@ -32,22 +37,25 @@ public class MarsRover {
     }
 
     private void executesCommand(String command) {
-        if (command.equals("M")) {
+        if (command.equals(MOVE)) {
             move();
-        } if (command.equals("L")){
+        }
+        if (command.equals(LEFT)) {
             turnLeft();
-        } if (command.equals("R")){}
+        }
+        if (command.equals(RIGHT)) {
+        }
 
     }
 
     private void turnLeft() {
-        if (heading.equals("N")){
-            heading = "W";
+        if (heading.equals(NORTH)) {
+            heading = WEST;
         }
     }
 
     private void move() {
-        if (heading.equals("N")) {
+        if (heading.equals(NORTH)) {
             locationY += 1;
         }
 
