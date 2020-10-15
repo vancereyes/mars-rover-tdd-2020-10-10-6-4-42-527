7e536dd6df44bf1d9895e4deaf13f1a6c225f7b2
VANCE ALBERT REYES (SHP-IRIS-ISD-OOCLL/MNL) <REYESVA@OOCL.COM>
2020-10-15 18:26:38 +0800

    [MarsRover][Vance]test - created test for 00E R

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -90,7 +90,7 @@ class MarsRoverTest {
     }
 
     @Test
-    void should_have_x_0_y_0_heading_N_when_command_L_given_x_0_y_0_heading_E() {
+    void should_have_x_0_y_0_heading_E_when_command_L_given_x_0_y_0_heading_N() {
         //given
         MarsRover marsRover = new MarsRover(0,0,"E");
         //when
@@ -99,6 +99,18 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getLocationX());
         assertEquals(0, marsRover.getLocationY());
         assertEquals("N", marsRover.getHeading());
+    }
+
+    @Test
+    void should_have_x_0_y_0_heading_E_when_command_R_given_x_0_y_0_heading_S() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"E");
+        //when
+        marsRover.executesCommands("R");
+        //then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("S", marsRover.getHeading());
     }
 
 
